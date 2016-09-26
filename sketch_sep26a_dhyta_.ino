39d3a6202591c70437def1a59344620bd48a8331
DhytaRizkitamaSusiloPutri <dhyta.rizqitamasp@gmail.com>
2016-09-26 14:15:39 +0700

    Add files via upload

---
+++ sketch_sep26a_dhyta_.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // initialize digital pin 11 as an output:
+  pinMode (11, OUTPUT);
+}
+
+void loop() {
+  digitalWrite (11, HIGH);    // turn the LED on (HIGH is the voltage level)
+  delay(2000);                // wait faor a second
+  digitalWrite(11, LOW);      // turn the LED off by making the voltage LOW
+  delay(2000);                // wait for a second   
+}
